ddf39a8198d76e8824b169c3eb13729fddc79b0e
Eric Good <ericdgood@yahoo.com>
2018-10-21 11:04:52 -0400

    used new api

--- app/src/main/java/com/example/edgoo/whatsgoingup/Utilities/FetchMovieData.java
+++ app/src/main/java/com/example/edgoo/whatsgoingup/Utilities/FetchMovieData.java
@@ -1,6 +1,5 @@
 package com.example.edgoo.whatsgoingup.Utilities;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -13,7 +12,6 @@ import org.json.JSONException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
 
 import static android.content.ContentValues.TAG;
 
@@ -32,7 +30,6 @@ public class FetchMovieData extends AsyncTask<String, Void, MoviesInfo[]> {
         URL movieUrl = null;
         try {
             movieUrl = getApiUrl();
-            Log.d(TAG, "doInBackground: " + movieUrl);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -40,9 +37,9 @@ public class FetchMovieData extends AsyncTask<String, Void, MoviesInfo[]> {
         try {
             assert movieUrl != null;
 //                BUILDS MOVIEDB URL INTO STRING URL
-            Log.d(TAG, "doInBackground pre fetch: " + movieUrl);
             String jsonresponse = FetchJson.getResponseFromHttpUrl(movieUrl);
-            Log.d(TAG, "doInBackground: " + jsonresponse);
+
+            Log.d(TAG, "parseMovieJson json: " + jsonresponse);
 //                PARES MOVIEDB URL
             return ParseMoviedb.parseMovieJson(jsonresponse);
 
@@ -57,20 +54,19 @@ public class FetchMovieData extends AsyncTask<String, Void, MoviesInfo[]> {
     private URL getApiUrl() throws MalformedURLException {
 //        https://api.aerisapi.com/forecasts/32920?filter=1hr&limit=999&client_id=HVptD2sYYxVqQ883K7Bkq&client_secret=u56EL25cFJ9GACs6oVTL2PgPBLYd9lkasSFTzxdl
 
-            final String MOVIEDB_URL = "https://launchlibrary.net/1.4/launch";
+            final String MOVIEDB_URL = "https://us-central1-whatsgoingup-spaceapschallenge.cloudfunctions.net/LoadLaunches";
         Uri baseUri = Uri.parse(MOVIEDB_URL);
         Uri.Builder uriBuilder = baseUri.buildUpon();
 //        uriBuilder.appendQueryParameter("api_key", API_KEY_PARAM);
 //        uriBuilder.appendQueryParameter("language", "en-US");
 //        uriBuilder.appendQueryParameter("page", "1");
 
-        Log.d(TAG, "getApiUrl: " + baseUri.toString());
+
         return new URL(uriBuilder.toString());
     }
 
     @Override
     protected void onPostExecute(MoviesInfo[] movieData) {
-        Log.d(TAG, "onPostExecute: " + Arrays.toString(movieData));
         mMovieAdapter.setMovieData(movieData);
     }
 }
--- app/src/main/java/com/example/edgoo/whatsgoingup/Utilities/ParseMoviedb.java
+++ app/src/main/java/com/example/edgoo/whatsgoingup/Utilities/ParseMoviedb.java
@@ -6,6 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import static android.content.ContentValues.TAG;
@@ -15,26 +16,21 @@ class ParseMoviedb {
     public static MoviesInfo[] parseMovieJson(String jsonResponse) throws JSONException {
 
 //          CREATES JSON OBJECT WITH JSON STRING
-        JSONObject mainJsonObject = new JSONObject(jsonResponse);
-//        GETS ARRAY OF MOVIE FROM RESULTS
-        JSONArray movieTitleArray = mainJsonObject.getJSONArray("launches");
-        Log.d(TAG, "logtag arr: " + movieTitleArray);
-//        ARRAY OF PARSED MOVIE INFO
-        MoviesInfo[] movie = new MoviesInfo[movieTitleArray.length()];
+        JSONArray mainJsonObject = new JSONArray(jsonResponse);
+        MoviesInfo[] movie = new MoviesInfo[mainJsonObject.length()];
 
 
 //                 LOOPS THROUGH EACH MOVE ARRAY RESULT
-        for (int i = 0; i < movieTitleArray.length(); i++) {
+        for (int i = 0; i < mainJsonObject.length(); i++) {
 
             movie[i] = new MoviesInfo();
 
 //                CREATES A CURRENT MOVIE OBJECT
-            JSONObject currentMovie = movieTitleArray.getJSONObject(i);
+            JSONObject currentMovie = mainJsonObject.getJSONObject(i);
 
 //            GETS OVERVIEW OF MOVIE
             movie[i].setRocket(currentMovie.getString("name"));
         }
-        Log.d(TAG, "logtag name: " + Arrays.toString(movie));
         return movie;
     }
 
